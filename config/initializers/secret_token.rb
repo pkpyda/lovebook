208b0cbf480080a51b12578e164bafbe761bdf15
pkpyda <pydapraveenkumar@gmail.com>
2014-07-17 14:19:11 +0100

    part1: created user model using devise and quote by scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovebook::Application.config.secret_token = '34628f39819eb913f574883ffd4a5719d5e1b1dd054cfe3e150caf3c4976e00e875cab0124248c9de4696991ff1edd88a2ba38805e889bb44231a654c36ef078'
